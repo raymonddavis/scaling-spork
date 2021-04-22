e870c52af508afb41c92cfdac49b39fcd0588c06
Raymond Davis IV <raymond.davis.4th@gmail.com>
2021-04-22 15:07:28 -0700

    Missing Exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Spork } from './Spork';
 import SporkHandler from './SporkHandler';
 import MissingSporkError from './MissingSporkError';
-import { ISporkOptions } from './ISporkOptions';
+import { DefaultOptions, ISporkOptions } from './ISporkOptions';
+import { ISpork } from './ISpork';
 
-export { Spork, SporkHandler, MissingSporkError, ISporkOptions };
+export {
+    Spork,
+    SporkHandler,
+    MissingSporkError,
+    DefaultOptions,
+    ISporkOptions,
+    ISpork,
+};
