a3d37b0864b8dc4e0ce13b6f5b538058ec0c8100
Raymond Davis IV <raymond.davis.4th@gmail.com>
2021-06-13 18:53:11 -0700

    Misspelled var

--- src/Spork.ts
+++ src/Spork.ts
@@ -8,12 +8,12 @@ export interface SporkMetaDataValue {
     options: ISporkOptions;
 }
 
-let SportCount = 0;
+let SporkCount = 0;
 
 export function Spork(options?: ISporkOptions) {
     return <T extends new (...args: {}[]) => object>(target: T) => {
         const metadataValue: SporkMetaDataValue = {
-            symbol: Symbol(`${SPORK_METADATA}-${target.name}-${SportCount++}`),
+            symbol: Symbol(`${SPORK_METADATA}-${target.name}-${SporkCount++}`),
             options: options || DefaultOptions,
         };
         Reflect.defineMetadata(SPORK_METADATA, metadataValue, target);
