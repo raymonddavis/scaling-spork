c4ef76244af24a7fec9ce50eda1b0f354e07bed3
Raymond Davis IV <raymond.davis.4th@gmail.com>
2021-06-30 05:09:25 -0700

    Update Node and rxjs and other packages

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -23,16 +23,16 @@ class ExampleEvent5 {}
 
 const sporkHandler = new SporkHandler();
 
-sporkHandler.on().subscribe((res) => console.log('logger: ', res));
+sporkHandler.on().subscribe((res: any) => console.log('logger: ', res));
 
 sporkHandler.dispatch(new ExampleEvent5());
 sporkHandler.dispatch(new ExampleEvent4());
 
-sporkHandler.on(ExampleEvent1).subscribe((res) => {
+sporkHandler.on(ExampleEvent1).subscribe((res: ExampleEvent1) => {
     console.log('1', res);
 });
 
-sporkHandler.on(ExampleEvent2).subscribe((res) => {
+sporkHandler.on(ExampleEvent2).subscribe((res: ExampleEvent2) => {
     console.log('2', res.data1);
 });
 
@@ -44,6 +44,8 @@ sporkHandler.dispatch(new ExampleEvent1());
 sporkHandler.dispatch(new ExampleEvent2({ test: 1 }));
 // sporkHandler.dispatch(new ExampleEvent3());
 
-sporkHandler.on(ExampleEvent4, { emitLast: true }).subscribe((res) => {
-    console.log('4', res);
-});
+sporkHandler
+    .on(ExampleEvent4, { emitLast: true })
+    .subscribe((res: ExampleEvent4) => {
+        console.log('4', res);
+    });
